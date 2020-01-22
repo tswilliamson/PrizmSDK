c83a6cbcdaf7405bcaccef91c441484c969a4a28
Thomas Williamson <thomas.williamson.dev@gmail.com>
2020-01-21 21:50:56 -0500

    Redirected winsim fxcg includes to core SDK includes so they fully match up

--- utils/winsim/fxcg/heap.h
+++ utils/winsim/fxcg/heap.h
@@ -1,17 +1,3 @@
-#ifndef _FXCG_HEAP_H
-#define _FXCG_HEAP_H
+#pragma once
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void *sys_calloc(int elements, int elementSize);
-void *sys_malloc(int sz);
-void *sys_realloc(void *p, int sz);
-void sys_free(void *p);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
+#include "../../include/fxcg/heap.h"
